0413380e137aff703ffa756a4fbb8111885c37c2
Theodor Müller <info@trilliarden.net>
2021-08-13 15:15:50 +0200

    Update manufacturer

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x444D
-#define MANUFACTURER    tshort
+#define MANUFACTURER    eiketheo
 
 /* mouse config */
 #define MOUSEKEY_INTERVAL       20
