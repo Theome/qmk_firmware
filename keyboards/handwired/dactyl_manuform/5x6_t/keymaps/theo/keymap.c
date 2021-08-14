721361d70fcef9cf2f52ee1d9d6b29e47a12cd37
Theodor Müller <info@trilliarden.net>
2021-08-14 14:38:17 +0200

    Update Theos Keymap

--- keyboards/handwired/dactyl_manuform/5x6_t/keymaps/theo/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_t/keymaps/theo/keymap.c
@@ -7,21 +7,23 @@
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _MAC 3
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define MAC MO(_MAC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-     RGB_TOG, KC_LBRC,KC_RBRC,KC_LEFT,   KC_RGHT,                                     KC_UP, KC_DOWN, KC_LBRC,  KC_RBRC, KC_EQL,
-                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                            KC_HOME,                         KC_END,
-                                KC_BSPC,KC_DEL, KC_GRV,                        KC_LGUI, KC_ENT, KC_SPC
+     KC_EQL , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
+     KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+     RGB_TOG, KC_GRV,KC_NUBS,KC_LEFT, KC_RGHT,                                        KC_UP, KC_DOWN, KC_LBRC,  KC_RBRC, RGB_TOG,
+                                      KC_LGUI,MAC,                          LOWER, KC_RGUI,
+                                            KC_LALT,                          KC_RALT,
+                                KC_BSPC,KC_DEL, KC_LCTRL,                     KC_RCTRL, KC_ENT, KC_SPC
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -38,6 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
+       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+       _______,_______,_______,_______,_______,                                         KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                                               _______,_______,            KC_EQL ,_______,
+                                                       _______,            _______,
+                                       _______,_______,_______,            _______,_______,_______
+  ),
+
+    [_MAC] = LAYOUT_5x6(
+       _______,KC_BRID , KC_BRIU , KC_MUTE , KC_VOLD , KC_VOLU,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
        _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
