9cabfc4fbe74dbe4c0bfc1d562cf12535eabc12d
Eike <bintux@gmail.com>
2021-08-26 21:24:58 +0200

    ADDED Nav, RBG and Special Layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
@@ -5,10 +5,16 @@
 
 #define _QWERTY 0
 #define _LOWER 1
-#define _RAISE 2
+#define _NAVLAY 2
+#define _RGBLAY 3
+#define _RAISE 4
+#define _SPECIAL 5
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define NAVLAY TT(_NAVLAY)
+#define RGBLAY MO(_RGBLAY)
+#define SPECIAL TT(_SPECIAL)
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
@@ -20,20 +26,20 @@ void keyboard_post_init_user(void) {
 
   rgblight_enable_noeeprom(); // enables Rgb, without saving settings
   rgblight_sethsv_noeeprom(180, 255, 255); // sets the color to teal/cyan without saving
-  rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3); // sets mode to Fast breathing
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 2); // sets mode to Fast breathing
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,LT(RGBLAY,KC_BSPC),
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  , KC_LBRC ,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
+     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RCTL,
+                      KC_LGUI,KC_LALT,                                                       KC_RALT, KC_RGUI,
                                        KC_SPC,KC_BSPC,                         LOWER,KC_ENT,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                      RAISE, RGB_TOG,                        KC_LGUI, KC_LALT
+                                      SPECIAL,NAVLAY,                         KC_END,  KC_DEL,
+                                      RAISE, RGBLAY,                        KC_LGUI, KC_LALT
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -42,7 +48,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
      _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                             _______,KC_PSCR,            _______, KC_P0,
+                     _______, KC_PSCR ,																									      _______, KC_P0,
+                                             _______,_______,           _______,_______,
+                                            _______ , _______ ,				  _______,_______,
+																						_______ ,_______,					  _______,_______
+
+  ),
+  [_NAVLAY] = LAYOUT_5x6(
+
+     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+     _______,_______,_______,KC_MS_U,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+     _______,_______,KC_MS_L,KC_MS_D,KC_MS_R,KC_LPRN,                        KC_LEFT, KC_DOWN , KC_UP , KC_RGHT ,KC_MINS,KC_PIPE,
+     _______,_______,_______,_______,_______,_______,                        KC_MPRV,KC_MPLY,KC_MNXT,_______ ,_______, KC_UNDS,
+                      _______,KC_PSCR,																						 						_______, KC_P0,
+                                             KC_BTN1,KC_BTN2,            KC_BTN1,KC_BTN2,
+                                             _______,_______,            _______,_______,
+                                             _______,_______,            _______,_______
+
+  ),
+  [_RGBLAY] = LAYOUT_5x6(
+
+     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                                             _______,RGB_TOG,            _______, KC_P0,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______
@@ -50,6 +80,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT_5x6(
+       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
+       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+                                               _______,_______,            KC_EQL ,_______,
+                                               _______,_______,            _______,_______,
+                                               _______,_______,            _______,_______,
+                                               _______,_______,            _______,_______
+  ),
+  [_SPECIAL] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
        _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
