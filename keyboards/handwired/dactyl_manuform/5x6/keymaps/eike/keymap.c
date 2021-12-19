df48450516e1a228c77d7963db2248a3d5ff1277
Eike <bintux@gmail.com>
2021-12-19 16:52:18 +0100

    FIXES Correcte PG UP and PG Down placements

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
@@ -36,35 +36,35 @@ void keyboard_post_init_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_GESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,LT(RGBLAY,KC_MINS),
+     KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,LT(RGBLAY,KC_MINS),
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  , KC_LBRC ,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RCTL,
                       KC_LGUI,KC_LALT,                                                       KC_RALT, KC_RGUI,
                                        KC_SPC,KC_BSPC,                        KC_ESC,KC_ENT,
-                                       LOWER,NAVLAY,                         NAVLAY,  LOWER,
-                                      _______, _______,                        KC_LGUI, KC_LALT
+                                       LOWER,NAVLAY,                          NAVLAY, LOWER,
+                                       KC_LALT, KC_LSFT,                      KC_RSFT , KC_LGUI
   ),
 
   [_LOWER] = LAYOUT_5x6(
 
      KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                         KC_P7 , KC_P8 , KC_P9 ,RALT(KC_7),RALT(KC_0),KC_PLUS,
-     KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,                         KC_P4 , KC_P5 , KC_P6 ,LSFT(KC_8),LSFT(KC_9),KC_MINS,
-     _______,_______,_______,_______,_______,_______,                         KC_P1 , KC_P2 , KC_P3 ,KC_NUBS, LSFT(KC_NUBS),_______,
-                     _______, _______ ,																									      KC_P0, _______,
+     KC_TILD,KC_EXLM, RALT(KC_7) ,RALT(KC_0),KC_DLR ,KC_PERC,                         KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,KC_ASTR,KC_PLUS,
+     KC_CIRC,KC_AMPR,LSFT(KC_8),LSFT(KC_9),KC_NUHS,RALT(KC_Q),                         KC_P4 , KC_P5 , KC_P6 ,KC_TILD,KC_PSLS,KC_MINS,
+     _______,_______,RALT(KC_8) ,RALT(KC_9),LSFT(KC_2),LSFT(KC_NUHS),                         KC_P1 , KC_P2 , KC_P3 ,KC_PDOT, KC_PEQL,_______,
+                     KC_NUBS, LSFT(KC_NUBS) ,																									      KC_P0, _______,
                                              _______,_______,           _______,_______,
                                             _______ , _______ ,				  _______,_______,
-																						_______ ,KC_PSCR,					  _______,_______
+																						_______ , _______,					  _______,_______
 
   ),
   [_NAVLAY] = LAYOUT_5x6(
 
-     KC_NO	,KC_NO	, KC_NO ,KC_WH_U,KC_NO	,KC_NO,                        KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_MUTE,
-     KC_NO, KC_NO,KC_NO,KC_MS_U,KC_NO,KC_NO,                        KC_HOME,KC_PGUP,KC_PGDN,KC_END,_______,KC_VOLU,
-     _______,_______,KC_MS_L,KC_MS_D,KC_MS_R,KC_NO,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,KC_MINS,KC_VOLD,
-     _______,_______,KC_WH_L,KC_WH_D,KC_WH_R,_______,                        KC_MPRV,KC_MPLY,KC_MNXT,_______ ,_______,_______ ,
-                      _______,_______,																						 						_______,_______,
+     KC_NO	,KC_NO	, KC_NO ,KC_WH_U,KC_NO	,KC_NO,                        KC_PSCR,KC_SLCK,KC_PAUS,KC_NLCK,KC_NO,KC_MUTE,
+     KC_NO, KC_NO,KC_NO,KC_MS_U,KC_NO,KC_NO,															 KC_HOME,KC_PGDN,KC_PGUP,KC_END,KC_NO,KC_VOLU,
+     _______,KC_NO,KC_MS_L,KC_BTN3,KC_MS_R,KC_NO,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,KC_MINS,KC_VOLD,
+     _______,KC_NO,KC_WH_L,KC_MS_D,KC_WH_R,KC_NO,                        KC_MPRV,KC_MPLY,KC_MNXT,KC_NO ,KC_NO,KC_NO ,
+                      _______,KC_WH_D,																						 						_______,_______,
                                              KC_BTN1,KC_BTN2,            KC_BTN1,KC_BTN2,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______
