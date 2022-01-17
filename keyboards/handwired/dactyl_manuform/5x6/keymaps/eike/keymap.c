f5b88313820bc738c17d7a45a42d391df2bda10d
Eike <bintux@gmail.com>
2022-01-17 08:24:08 +0100

    FIXES
    Corrected incorrect keymappings for the numblock layer
    Added Del on second layer for backspace

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_5x6(
 
      KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-     KC_TILD,KC_EXLM, RALT(KC_7) ,RALT(KC_0),KC_DLR ,KC_PERC,                         KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,KC_ASTR,KC_PLUS,
-     KC_CIRC,KC_AMPR,LSFT(KC_8),LSFT(KC_9),KC_NUHS,RALT(KC_Q),                         KC_P4 , KC_P5 , KC_P6 ,KC_TILD,KC_PSLS,KC_MINS,
+     KC_TILD,KC_EXLM, RALT(KC_7) ,RALT(KC_0),KC_DLR ,KC_PERC,                         KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,KC_PAST,KC_PPLS,
+     KC_CIRC,KC_AMPR,LSFT(KC_8),LSFT(KC_9),KC_NUHS,RALT(KC_Q),                         KC_P4 , KC_P5 , KC_P6 ,RALT(KC_RBRC),KC_PSLS,KC_PMNS,
      _______,_______,RALT(KC_8) ,RALT(KC_9),LSFT(KC_2),LSFT(KC_NUHS),                         KC_P1 , KC_P2 , KC_P3 ,KC_PDOT, KC_PEQL,_______,
                      KC_NUBS, LSFT(KC_NUBS) ,																									      KC_P0, _______,
-                                             _______,_______,           _______,_______,
+                                             _______,KC_DEL,           _______,_______,
                                             _______ , _______ ,				  _______,_______,
 																						_______ , _______,					  _______,_______
 
