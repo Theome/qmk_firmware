205a5ec250027a7e63d127b0741fc0c4cf1bf99e
Eike <bintux@gmail.com>
2022-09-16 21:33:19 +0200

    Changed Layout based on feedback from Theo

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/eike/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_5x6(
      KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0   ,LT(RGBLAY,KC_MINS),
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P   , KC_LBRC ,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN ,KC_QUOT,
+     KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN ,KC_QUOT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH ,KC_RCTL,
                       KC_LGUI,KC_LALT,                                                       KC_RALT, KC_RGUI,
-                                       KC_SPC,KC_BSPC,                        KC_ESC,KC_ENT,
+                                       KC_SPC,KC_BSPC,                        KC_DEL,KC_ENT,
                                        LOWER,NAVLAY,                          NAVLAY, LOWER,
                                        KC_LALT, KC_LSFT,                      KC_RSFT , KC_LGUI
   ),
@@ -52,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_5x6(
 
      KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6 , KC_F7 , KC_F8 , KC_F9         , KC_F10  , KC_F11 ,
-     RALT(KC_RBRC),KC_EXLM, RALT(KC_7) ,RALT(KC_0),KC_DLR ,KC_PERC,					 KC_P7 , KC_P8 , KC_P9 , KC_NLCK       , KC_PAST , KC_PPLS,
-     KC_CIRC,KC_AMPR,LSFT(KC_8),LSFT(KC_9),KC_NUHS,RALT(KC_Q),							 KC_P4 , KC_P5 , KC_P6 , RALT(KC_MINS) , KC_PSLS , KC_PMNS,
-     _______,KC_PEQL,RALT(KC_8) ,RALT(KC_9),LSFT(KC_2),LSFT(KC_NUHS),        KC_P1 , KC_P2 , KC_P3 , TD(TAPDANCE_1) , KC_PEQL  , _______,
+     KC_PSLS ,KC_EXLM, RALT(KC_7) ,RALT(KC_0),KC_DLR ,RALT(KC_RBRC),				 KC_NLCK       , KC_P7 , KC_P8 , KC_P9 , KC_PAST , KC_PPLS,
+     RALT(KC_MINS) , RALT(KC_Q),LSFT(KC_8),LSFT(KC_9),LSFT(KC_EQL),KC_EQL,	 KC_NUHS , KC_P4 , KC_P5 , KC_P6 , KC_PSLS , KC_PMNS,
+     _______,RALT(KC_NUBS),RALT(KC_8) ,RALT(KC_9),LSFT(KC_2),LSFT(KC_NUHS),  KC_PEQL  ,KC_P1 , KC_P2 , KC_P3 , TD(TAPDANCE_1) ,  _______,
                      KC_NUBS, LSFT(KC_NUBS) ,																									      KC_P0, _______,
-                                             _______,KC_DEL,           _______,_______,
+                                             _______,_______,           _______,_______,
                                             _______ , _______ ,				  _______,_______,
 																						_______ , _______,					  _______,_______
 
